e8dce5ac33042a9bf894e4f2afc2ae11ee8ddac5
gyan3x <gyandev.m@simplify3x.com>
2023-05-12 13:21:10 +0530

    router created with opening message

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+const { urlencoded } = require("express");
 const express = require("express");
 const app = express();
+app.use(express.json());
+app.use(urlencoded({extended:false}))
 
+app.use('/megabot/v1.0/',require('./routes/chat'))
 
 
 app.listen(3000,()=>{
